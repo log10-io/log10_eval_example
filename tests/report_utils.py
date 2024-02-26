0b45b9cd07919fea72553a2914a435d57797224a
Wenzhe Xue <wenzhe@log10.io>
2024-02-25 21:40:05 -0800

    updates: - refactor report util func - add a fig to the report - update cosin distance - regenerate reports

---
+++ tests/report_utils.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+from tabulate import tabulate
+from typing import List, Tuple
+
+
+def filter_results_by_test_name(
+    module_results_df: pd.DataFrame, test_name_prefix: str
+) -> pd.DataFrame:
+    filtered_df = module_results_df[
+        module_results_df["test_name"].str.contains(test_name_prefix)
+    ]
+    return filtered_df
+
+
+def report_pass_rate(dataframe: pd.DataFrame) -> Tuple[float, str]:
+    pass_number = len(dataframe[dataframe["status"] == "passed"])
+    total_runs = len(dataframe)
+    pass_rate = pass_number / total_runs * 100
+    report_str = f"## Test Pass Rate\n Pass rate {pass_rate:.1f}% ({pass_number}/{total_runs})"
+
+    return pass_rate, report_str
+
+
+def generate_results_table(dataframe: pd.DataFrame, column_list: list[str]) -> str:
+    selected_df = dataframe[column_list]
+    table = tabulate(selected_df, headers="keys", tablefmt="pipe", showindex=True)
+    ret_str = f"## Test Results\n{table}"
+    return ret_str
+
+
+def generate_markdown_report(test_name: str, report_strings: List[str]):
+    output_dir = os.environ.get("OUTPUT_REPORT_DIR", "generated_reports")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    with open(f"{output_dir}/{test_name}.md", "w") as f:
+        f.write(f"Generated from {test_name}.\n\n")
+        for report_string in report_strings:
+            f.write(report_string + "\n\n")
--- tests/test_my_llm.py
+++ tests/test_my_llm.py
@@ -1,6 +1,8 @@
 import pytest
 import sys
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Append the src directory to sys.path to make its modules available for import
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
@@ -16,6 +18,12 @@ from my_llm import (
     sys_message_2,
 )
 from my_eval_metrics import cosine_distance, count_words
+from report_utils import (
+    filter_results_by_test_name,
+    report_pass_rate,
+    generate_results_table,
+    generate_markdown_report,
+)
 
 
 @pytest.fixture
@@ -55,7 +63,7 @@ def test_simple(article, expected_summary, results_bag):
     assert metric < 0.2
 
 
-def test_mean_cosine_similarity(module_results_df):
+def test_mean_cosine_similarity(module_results_df: pd.DataFrame):
     print("Average cosine distance: ", module_results_df["cos_sim"].mean())
     print("Std dev of cosine distance: ", module_results_df["cos_sim"].std())
 
@@ -64,7 +72,7 @@ def test_mean_cosine_similarity(module_results_df):
 
 @pytest.mark.repeat(2)
 @pytest.mark.parametrize("sample_idx", range(3))
-def test_summarize_to_30_words(data, sample_idx, results_bag):
+def test_summarize_to_30_words(data: list, sample_idx: int, results_bag):
     article, expected_summary = data[sample_idx]
     output = summarize_to_30_words(article)
     metric = cosine_distance(expected_summary, output)
@@ -80,51 +88,51 @@ def test_summarize_to_30_words(data, sample_idx, results_bag):
     assert num_words <= 30
 
 
-def test_pass_rate_of_30_words(module_results_df):
-    df = module_results_df[
-        module_results_df["test_name"].str.contains("test_summarize_to_30_words")
+def _prepare_long_string_section(dataframe: pd.DataFrame, column_name: str) -> str:
+    dataframe[f"{column_name}_long"] = dataframe[column_name]
+    dataframe[f"{column_name}_id"] = dataframe["test_name"].str.split("_").str[-1]
+    dataframe[column_name] = dataframe.apply(
+        lambda row: row[f"{column_name}_long"][:50]
+        + f"... [more](#article-{row[f'{column_name}_id']})",
+        axis=1,
+    )
+
+    # get unique items
+    unique_items = dataframe[f"{column_name}_long"].unique()
+    long_items_section = f"## {column_name.capitalize()} details\n\n"
+    for i, text in enumerate(unique_items):
+        long_items_section += f"### {column_name.capitalize()} {i}\n"
+        long_items_section += f"{text}\n\n"
+
+    return long_items_section
+
+
+def test_pass_rate_of_30_words(module_results_df: pd.DataFrame):
+    df = filter_results_by_test_name(module_results_df, "test_summarize_to_30_words")
+
+    pass_rate, pass_rate_report_str = report_pass_rate(df)
+    long_articles_section = _prepare_long_string_section(df, "article")
+    selected_columns = [
+        "status",
+        "article",
+        "expected_summary",
+        "output",
+        "cos_sim",
+        "num_words",
     ]
-    pass_rate = len(df[df["status"] == "passed"]) / len(df)
+    detailed_results = generate_results_table(df, selected_columns)
 
-    df["article_long"] = df["article"]
-    df["article_id"] = df["test_name"].str.split("_").str[-1]
-    df["article"] = df.apply(lambda row: row["article_long"][:50] + f"... [more](#article-{row['article_id']})", axis=1)
+    generate_markdown_report(
+        "test_summarize_to_30_words",
+        [pass_rate_report_str, detailed_results, long_articles_section],
+    )
 
-    # get unique articles
-    unique_articles = df["article_long"].unique()
-    # import ipdb; ipdb.set_trace()
-    long_articles_section = "## Long Articles\n\n"
-    for i, text in enumerate(unique_articles):
-        long_articles_section += f"### Article {i}\n"
-        long_articles_section += f"{text}\n\n"
-
-    selected_df = df[
-        [
-            "status",
-            "article",
-            "expected_summary",
-            "output",
-            "cos_sim",
-            "num_words",
-        ]
-    ]
-    table = tabulate(selected_df, headers="keys", tablefmt="pipe", showindex=True)
-
-    output_dir = os.environ.get("OUTPUT_REPORT_DIR", "generated_reports")
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    with open(f"{output_dir}/test_30_words_table.md", "w") as f:
-        f.write("Generated from test_my_llm::test_pass_rate_of_30_words\n\n")
-        f.write(f"## Test Pass Rate\nPass rate: {pass_rate * 100:.1f}%\n\n")
-        f.write("## Detailed Results\n\n")
-        f.write(table + "\n\n")
-        f.write(long_articles_section)
     assert pass_rate > 0.66
 
 
 @pytest.mark.repeat(3)
 @pytest.mark.parametrize("sample_idx", range(3))
-def test_summarize_with_sys_prompt_1(data, sample_idx, results_bag):
+def test_summarize_with_sys_prompt_1(data: list, sample_idx: int, results_bag):
     article, expected_summary = data[sample_idx]
     output = summarize_with_sys_prompt_1(article)
     metric = cosine_distance(expected_summary, output)
@@ -139,7 +147,7 @@ def test_summarize_with_sys_prompt_1(data, sample_idx, results_bag):
 
 @pytest.mark.repeat(3)
 @pytest.mark.parametrize("sample_idx", range(3))
-def test_summarize_with_sys_prompt_2(data, sample_idx, results_bag):
+def test_summarize_with_sys_prompt_2(data: list, sample_idx: int, results_bag):
     article, expected_summary = data[sample_idx]
     output = summarize_with_sys_prompt_2(article)
     metric = cosine_distance(expected_summary, output)
@@ -152,54 +160,48 @@ def test_summarize_with_sys_prompt_2(data, sample_idx, results_bag):
     results_bag.prompt = sys_message_2
 
 
-def test_compare_prompts_results(module_results_df):
+def test_compare_prompts_results(module_results_df: pd.DataFrame):
     df = module_results_df[
         module_results_df["test_name"].str.contains("test_summarize_sys_prompt_")
     ]
     # save df to csv
     df.to_csv("module_results_df.csv", index=True)
+
+    # compare mean and std of cosine distance
     mean_1 = df[df["test_name"].str.contains("sys_prompt_1")]["cos_sim"].mean()
     std_1 = df[df["test_name"].str.contains("sys_prompt_1")]["cos_sim"].std()
     mean_2 = df[df["test_name"].str.contains("sys_prompt_2")]["cos_sim"].mean()
     std_2 = df[df["test_name"].str.contains("sys_prompt_2")]["cos_sim"].std()
 
+    # Create a new dataframe
+    summary_df = pd.DataFrame({
+        "Prompt": ["sys_prompt_1", "sys_prompt_2"],
+        "Mean Cosine distance": [mean_1, mean_2],
+        "Std Dev": [std_1, std_2]
+    })
+
+    # plot summary_df to a bar chart and save to file
+    plot_file = "generated_reports/test_compare_prompts_results.png"
+    summary_df.plot(kind="bar", x="Prompt", y="Mean Cosine distance", yerr="Std Dev")
+    plt.savefig(plot_file)
+
+    markdown_table= tabulate(summary_df, headers="keys", tablefmt="pipe", showindex=False)
+    prompt_comp_section = "## Prompt Comparison\n\n" + markdown_table + "\n\n![Prompt Comparison](test_compare_prompts_results.png)"
+
     # remove new lines in output
     df["output"] = df["output"].str.replace("\n", " ")
-    df["article_long"] = df["article"]
-    df["article_id"] = df["test_name"].str.split("_").str[-1]
-    df["article"] = df.apply(lambda row: row["article_long"][:50] + f"... [more](#article-{row['article_id']})", axis=1)
 
-    unique_articles = df["article_long"].unique()
-    long_articles_section = "## Long Articles\n\n"
-    for i, text in enumerate(unique_articles):
-        long_articles_section += f"### Article {i}\n"
-        long_articles_section += f"{text}\n\n"
+    long_articles_section = _prepare_long_string_section(df, "article")
 
-    # generate a markdown table for the results
-    markdown_table = f"""
-| Prompt | Mean Cosine distance | Std Dev |
-|--------|------------------------| --------|
-| sys_prompt_1 | {mean_1:.3f} | {std_1:.3f} |
-| sys_prompt_2 | {mean_2:.3f} | {std_2:.3f} |
-"""
-    selected_df = df[
-        [
-            "prompt",
-            "article",
-            "expected_summary",
-            "output",
-            "cos_sim",
-        ]
+    selected_columns = [
+        "prompt",
+        "article",
+        "expected_summary",
+        "output",
+        "cos_sim",
     ]
-    table = tabulate(selected_df, headers="keys", tablefmt="pipe", floatfmt=".3f", showindex=False)
-    output_dir = os.environ.get("OUTPUT_REPORT_DIR", "generated_reports")
-    # check if the output directory exists and create it if it doesn't
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    with open(f"{output_dir}/prompt_comparison_report.md", "w") as f:
-        f.write("generated from test_my_llm::test_compare_prompts_results\n\n")
-        f.write("Compare the mean cosine distance of the two system prompts\n\n")
-        f.write(markdown_table + "\n\n")
-        f.write("## Detailed Results\n\n")
-        f.write(table + "\n\n")
-        f.write(long_articles_section)
+    detailed_results = generate_results_table(df, selected_columns)
+    generate_markdown_report(
+        "test_compare_prompts_results",
+        [prompt_comp_section, detailed_results, long_articles_section],
+    )
